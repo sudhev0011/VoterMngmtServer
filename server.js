2a93338a658f39729441795fb07ad5fc66b2fa1c
sudhev suresh <sudhevsuresh0011@gmail.com>
2025-08-06 02:03:05 +0530

    feat: Enable server listening on specified port and remove Vercel configuration

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api', require('./routes/todos'));
 
 app.get('/apis', (req, res) => res.json({ message: 'API is running' }));
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-export default app
+// export default app
