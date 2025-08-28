2b2ba17799cca70128bb0093e88284ab9e196eef
sudhev suresh <sudhevsuresh0011@gmail.com>
2025-08-28 14:51:22 +0530

    fix: Improve cookie setting logic to handle missing tokens and adjust SameSite attribute

--- utils/setCookie.js
+++ utils/setCookie.js
@@ -1,11 +1,19 @@
 const setCookie = (token, res)=>{
-
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'None',
-      maxAge: 24 * 60 * 60 * 1000, // 1 day
-    });
+    if(token){
+      try {
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'lax',
+          path: '/',
+          maxAge: 24 * 60 * 60 * 1000, // 1 day
+        });
+      } catch (error) {
+        console.log('cookie error:',error)
+      }
+    }else{
+      console.log("no token found for setting cookie")
+    }
 }
 
 module.exports = setCookie;
